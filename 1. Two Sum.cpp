c47eecce7943477cc0c72b24de0e1177148d4491
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-10-31 17:44:46 +0300

    1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,19 @@
+// 1. Two Sum : https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> our_map;
+        int diff_number;
+
+        for (int i = 0; i < nums.size(); i++) {
+            diff_number = target - nums[i];
+
+            if (our_map.count(diff_number)) {
+                return {our_map[diff_number], i};
+            }
+            our_map[nums[i]] = i;
+        }
+        return {-1};
+    }
+};
