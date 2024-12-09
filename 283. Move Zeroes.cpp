1338963662b18dc852968bd520037ef0c0b818eb
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-12-09 18:01:26 +0200

    283. Move Zeroes

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,18 @@
+// 283. Move Zeroes : https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+
+        for (int i = counter; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
