6b90b371ca529114746c63b2b3928053ac33740c
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-12-16 12:24:22 +0200

    704. Binary Search

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,24 @@
+// 704. Binary Search : https://leetcode.com/problems/binary-search/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
