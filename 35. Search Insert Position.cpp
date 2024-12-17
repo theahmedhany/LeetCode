6ccbffef5863211d8326091d157f62f6dc8150f8
Ahmed Hany <a7medhanyshokry@gmail.com>
2024-12-17 11:13:34 +0200

    35. Search Insert Position

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,24 @@
+// 35. Search Insert Position : https://leetcode.com/problems/binary-search/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
