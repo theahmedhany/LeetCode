8e35265d9d200abb6e7059a4feffcab119b036e3
Ahmed Hany <a7medhanyshokry@gmail.com>
2025-01-17 14:28:22 +0200

    141. Linked List Cycle

---
+++ 141. Linked List Cycle.cpp
@@ -0,0 +1,20 @@
+// 141. Linked List Cycle : https://leetcode.com/problems/linked-list-cycle/
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
