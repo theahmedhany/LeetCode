e39b87720c09aaa21ae31dbd2a17a30b6d39c7a8
Ahmed Hany <a7medhanyshokry@gmail.com>
2025-01-28 05:29:57 +0200

    112. Path Sum

---
+++ 112. Path Sum.cpp
@@ -0,0 +1,18 @@
+// 112. Path Sum : https://leetcode.com/problems/path-sum/
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == NULL) {
+            return false;
+        }
+
+        if (root->val == targetSum && root->left == NULL &&
+            root->right == NULL) {
+            return true;
+        }
+
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
